74080f9859b23b66e6c4205e3514f5a78bd205a8
mishanya <mishanyaxiq@gmail.com>
2024-01-22 19:48:14 +0100

    Configuring vue

--- cookingd-front/src/App.vue
+++ cookingd-front/src/App.vue
@@ -1,19 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from '@/components/Navbar.vue'
 </script>
 
 <template>
   <header>
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <NavBar/>
   </header>
 
   <RouterView />
---
+++ cookingd-front/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <ul>
+    <li><RouterLink to="/">Home</RouterLink></li>
+    <li><RouterLink to="/my-account">My Account</RouterLink></li>
+    <li><RouterLink to="/About">About</RouterLink></li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
